802882ad665a7953fc6b527e88ccd06c7977745e
Tristan Hearn <tristanhearn@gmail.com>
2017-02-19 23:17:00 -0500

    first commit

---
+++ gradle/__init__.py
@@ -0,0 +1,1 @@
+from gradle import gradle
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+config = {
+    'description': 'Gradle test runner',
+    'version': '0.1',
+    'packages': ['gradle'],
+    'scripts': [],
+    'name': 'pygradle'
+}
+
+setup(**config)
